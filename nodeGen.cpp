019ad740a3d6f42b6684b8d2d15779505db1f0cd
bryanxiong <bxiong17@my.whitworth.edu>
2016-12-13 16:35:19 -0800

    Fixed my nodeGen

--- nodeGen.cpp
+++ nodeGen.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
-#include <fstream>
-#include <string>
 #include <stdio.h>
 
 using namespace std;
 
 int main()
-{
-	//ofstream myfile;
-	//myfile.open("nodes.txt");
-	
+{	
 	FILE *myfile = fopen("nodes.txt", "w");
 	
 	srand(time(NULL));
@@ -19,22 +14,16 @@ int main()
 	
 	cout << "Num of Nodes? ";
 	cin >> n;
-	//string nodes[n];
-	//int temp = n;
 	
-	//for (temp; temp > 0; temp--)
 		for(int i = 1; i <= n; i++)
 		{
 			int nodes = rand() % n + 1;
 			float weights = (float)rand() / (float)RAND_MAX;
 			//cout << index++ << " " << nodes << " " <<  weights << endl;
 			fprintf(myfile, "%d\t%d\t%f\r\n", index++, nodes,weights);
-			//myfile << endl;
 			
 		}
 		fclose(myfile);
-	//myfile.close();
-	
 	
 	return 0;
 }
